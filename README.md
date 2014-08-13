97efda09e36872008a266d19e080ca43d7dd36c6
Dominykas Blyžė <hello@dominykas.com>
2014-08-13 17:19:27 +0300

    `release` task

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 grunt-iw-release
 ================
 
-InsideWarehouse release/deploy flow
+InsideWarehouse release/deploy flow.
+
+## `release` task
+
+1. Checks you're on `master` branch
+2. Checks there are no pending changes
+3. Bumps version (you need to provide `--bump=patch|minor|major`), updates `package.json`
+4. Calls `build`
+   * `build` should be defined to clean up, download/update any necessary data and generate the final bundle for `compress`
+5. Calls `compress`
+   * `compress` needs to be configured to produce the final `build/%APPNAME%-v%VERSION%.tgz`
+   * Inside the archive there should be onlye one folder: `%APPNAME%-v%VERSION%`
+6. If everything succeeds - commits the changes, tags the release and pushes to `origin`
+   * If the Gruntfile is inside the git repo root - the tag/commit will be as if `npm version` was called
+   * If the Gruntfile is in a subfolder, the tag/commit will be prefixed with `%APPNAME%`
+
+### TODO
+
+* Gracefully revert if `build`/`compress` fail
+
+## `deploy` task
+
+* Someday later :)
+
---
+++ tasks/release.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+
+	grunt.registerTask("release", "Prepare release bundle (bump version, build, compress, commit, tag, push)", function () {
+		var bumpLevel = grunt.option("bump");
+		if (bumpLevel !== "minor" && bumpLevel !== "major" && bumpLevel !== "patch") {
+			grunt.fail.fatal("Please provide `--bump=patch|minor|major`");
+			return;
+		}
+
+		var shell = require("shelljs");
+
+		var bumpOptions = {
+			updateConfigs: [ "pkg" ],
+			pushTo: "origin",
+			commitFiles: [ "-a" ],
+			commitMessage: "%VERSION%",
+			tagName: "v%VERSION%",
+			tagMessage: "%VERSION%"
+		};
+		var toplevelOutput = shell.exec("git rev-parse --show-toplevel", { silent: !!grunt.option('verbose') });
+		if (toplevelOutput.code !== 0) {
+			grunt.fail.fatal("Failed to detect top level git folder");
+		}
+
+		var topLevelPath = toplevelOutput.output.trim();
+		if (grunt.file.isPathCwd(topLevelPath)) {
+			var pkgName = grunt.config.get("pkg.name");
+			bumpOptions.commitMessage = "[" + pkgName + "] v%VERSION%";
+			bumpOptions.tagName = pkgName + "-v%VERSION%";
+			bumpOptions.tagMessage = "Build tag: " + pkgName + "-v%VERSION%";
+		}
+
+		grunt.config.set("bump", { options: bumpOptions });
+
+		grunt.task.run("checkbranch:master");
+		grunt.task.run("checkpending");
+
+		grunt.task.run("bump-only:" + bumpLevel);
+
+		grunt.task.run("build");
+		grunt.task.run("compress");
+
+		grunt.task.run("bump-commit");
+	});
+
+};
